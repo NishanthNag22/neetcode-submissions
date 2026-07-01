4798f6705f0bbfaf98adf1ae90433902481e6899
Nishanth Nag M V <pes1ug24am381@pesuonline.onmicrosoft.com>
2026-07-01 11:05:39 +0530

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,23 @@
+class TimeMap:
+    def __init__(self):
+        self.stack={}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.stack:
+            self.stack[key]=[]    
+        self.stack[key].append([timestamp,value])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.stack:
+            return ""
+        arr=self.stack[key]
+        low,high=0,len(arr)-1
+        res=''
+        while low<=high:
+            mid=(low+high)//2
+            if arr[mid][0]<=timestamp:
+                res=arr[mid][1]
+                low=mid+1
+            else:
+                high=mid-1
+        return res
